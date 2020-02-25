e2fbbad60413d020de98aa51ad9ce391a9f4acf0
michaelmcguiness <michaelmcguiness123@gmail.com>
2020-02-25 13:41:17 -0500

    finished sql alchemy

--- flask-SQLAlchemy/code/resources/item.py
+++ flask-SQLAlchemy/code/resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -8,6 +7,8 @@ class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('price', type=float, required=True,
                         help="This field cannot be left blank!")
+    parser.add_argument('store_id', type=int, required=True,
+                        help="Every item needs a store id")
 
     @jwt_required()
     def get(self, name):
@@ -21,7 +22,7 @@ class Item(Resource):
             return {"message": "An item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, data['price'], data['store_id'])
 
         try:
             item.save_to_db()
@@ -43,7 +44,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
 
@@ -54,15 +55,5 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-
-        connection.close()
-
-        return {'items': items}
+        # could also use list comprehension: [item.json() for item in ItemModel.query.all()]
+        return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
