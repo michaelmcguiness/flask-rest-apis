8850a7e8059bd8dfec61f8f039a4c23fd28dedda
michaelmcguiness <michaelmcguiness123@gmail.com>
2020-02-27 13:46:35 -0500

    finished final project

--- final_project/code/resources/item.py
+++ final_project/code/resources/item.py
@@ -1,5 +1,12 @@
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
+from flask_jwt_extended import (
+    jwt_required,
+    jwt_optional,
+    get_jwt_claims,
+    get_jwt_identity,
+    fresh_jwt_required
+)
+
 from models.item import ItemModel
 
 
@@ -10,13 +17,14 @@ class Item(Resource):
     parser.add_argument('store_id', type=int, required=True,
                         help="Every item needs a store_id")
 
-    @jwt_required()
+    @jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
             return item.json()
         return {'message': 'Item not found'}, 404
 
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {"message": "An item with name '{}' already exists".format(name)}, 400
@@ -33,7 +41,12 @@ class Item(Resource):
 
         return item.json(), 201
 
+    @jwt_required
     def delete(self, name):
+        claims = get_jwt_claims()
+        if not claims['is_admin']:
+            return {'message': 'Admin privilege required.'}, 401
+
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
@@ -54,7 +67,15 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_optional
     def get(self):
+        user_id = get_jwt_identity()
         # could return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
         # but list comprehensions are faster
-        return {'items': [x.json() for x in ItemModel.find_all()]}
+        items = [x.json() for x in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {
+            'items': [item['name'] for item in items],
+            'message': 'More data available if you log in.'
+        }, 200
