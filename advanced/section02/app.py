ac6d0dfed16f57458b993cf191243d24baefb155
michaelmcguiness <michaelmcguiness123@gmail.com>
2020-03-01 14:15:35 -0500

    finished serialization with marshmallow

--- advanced/section02/app.py
+++ advanced/section02/app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from marshmallow import ValidationError
 
+from ma import ma
 from db import db
 from blacklist import BLACKLIST
 from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout
@@ -25,6 +27,9 @@ api = Api(app)
 def create_tables():
     db.create_all()
 
+@app.error_handler(ValidationError)
+def handle_marshmallow_validation(err): # except ValidationError as err
+    return jsonify(err.messages), 400
 
 jwt = JWTManager(app)
 
@@ -33,7 +38,6 @@ jwt = JWTManager(app)
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     return decrypted_token["jti"] in BLACKLIST
-
 
 api.add_resource(Store, "/store/<string:name>")
 api.add_resource(StoreList, "/stores")
@@ -47,4 +51,5 @@ api.add_resource(UserLogout, "/logout")
 
 if __name__ == "__main__":
     db.init_app(app)
+    ma.init_app(app)
     app.run(port=5000, debug=True)
--- advanced/section02/models/user.py
+++ advanced/section02/models/user.py
@@ -9,15 +9,10 @@ class UserModel(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(80))
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, username: str, password: str):
-        self.username = username
-        self.password = password
-
-    def json(self) -> UserJSON:
-        return {"id": self.id, "username": self.username}
+    # __init__ method would be redundant
 
     @classmethod
     def find_by_username(cls, username: str) -> "UserModel":
@@ -34,3 +29,4 @@ class UserModel(db.Model):
     def delete_from_db(self) -> None:
         db.session.delete(self)
         db.session.commit()
+
