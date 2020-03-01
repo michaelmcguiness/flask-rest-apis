01c117ab8f98440662a337345c317f59cb62b450
michaelmcguiness <michaelmcguiness123@gmail.com>
2020-03-01 16:34:16 -0500

    finished email confirmation part 1

--- advanced/section02/pipenv_notes.md
+++ advanced/section02/pipenv_notes.md
@@ -1,10 +1,12 @@
 # Pipenv
- - set virtual environment to pipenv
- - open pycharm Preferences, go to Project Interpreter and install packages:
-    - Flask
-    - Flask-JWT-Extended
-    - Flask-RESTful
-    - Flask-SQLAlchemy
-    - flask-marshmallow
-    - marshmallow-sqlalchemy
-    - marshmallow
+
+- set virtual environment to pipenv
+- open pycharm Preferences, go to Project Interpreter and install packages:
+  - Flask
+  - Flask-JWT-Extended
+  - Flask-RESTful
+  - Flask-SQLAlchemy
+  - flask-marshmallow
+  - marshmallow-sqlalchemy
+  - marshmallow
+  - python-dotenv
--- advanced/section03/models/user.py
+++ advanced/section03/models/user.py
@@ -1,5 +1,7 @@
+from flask import request, url_for
+from requests import Response
 from db import db
-
+from libs.mailgun import Mailgun
 
 class UserModel(db.Model):
     __tablename__ = "users"
@@ -7,14 +9,28 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    activated = db.Column(db.Boolean, default=False)
 
     @classmethod
     def find_by_username(cls, username: str) -> "UserModel":
         return cls.query.filter_by(username=username).first()
 
     @classmethod
+    def find_by_email(cls, email: str) -> "UserModel":
+        return cls.query.filter_by(email=email).first()
+    @classmethod
     def find_by_id(cls, _id: int) -> "UserModel":
         return cls.query.filter_by(id=_id).first()
+
+    def send_confirmation_email(self) -> Response:
+        # http://127.0.0.1:5000/user_confirm/1
+        link = request.url_root[0:-1] + url_for("userconfirm", user_id=self.id)
+        subject = "Registration confirmation"
+        text= f"Please click the link to confirm your registration: {link}"
+        html = f'<html>Please click the link to confirm your registration: <a href="{link}">{link}</a></html>'
+
+        return Mailgun.send_email([self.email], subject, text, html)
 
     def save_to_db(self) -> None:
         db.session.add(self)
