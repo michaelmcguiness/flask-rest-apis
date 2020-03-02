c70a8be9fccb60eaef96694584ef390c2aef5168
michaelmcguiness <michaelmcguiness123@gmail.com>
2020-03-02 15:34:44 -0500

    finished advanced mail section

--- advanced/advancedemail/libs/strings.py
+++ advanced/advancedemail/libs/strings.py
@@ -16,7 +16,14 @@ def refresh():
     with open(f"strings/{default_locale}.json") as f:
         cached_strings = json.load(f)
 
+
 def gettext(name):
     return cached_strings[name]
+
+
+def set_default_locale(locale):
+    global default_locale
+    default_locale = locale
+
 
 refresh()
